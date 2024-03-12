1e2e3d16e0c0e88c1f4305472cc283aaf8914445
upron81 <77348627+upron81@users.noreply.github.com>
2024-03-12 14:25:57 +0300

    Ref 2

--- lab3/src/lab3/Main.java
+++ lab3/src/lab3/Main.java
@@ -7,43 +7,43 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Введите количество элементов в массиве:"); // Поменял вывод на русский
+        System.out.println("Введите количество элементов в массиве:"); // Изменение вывода на русский
         int n = scanner.nextInt();
 
         int[] array = new int[n];
 
-        System.out.println("Введите " + n + " целых чисел:"); // Поменял вывод на русский
+        System.out.println("Введите " + n + " целых чисел:"); // Изменение вывода на русский
         for (int i = 0; i < n; i++) {
             try {
                 array[i] = scanner.nextInt();
             } catch (InputMismatchException e) {
-                System.out.println("Пожалуйста, вводите только целые числа. Перезапустите программу и попробуйте снова."); // Поменял вывод на русский
+                System.out.println("Пожалуйста, вводите только целые числа. Перезапустите программу и попробуйте снова."); // Изменение вывода на русский
                 return;
             }
         }
 
         int numOfBuckets;
         while (true) {
-            System.out.println("Введите количество корзин:"); // Поменял вывод на русский
+            System.out.println("Введите количество корзин:"); // Изменение вывода на русский
             try {
                 numOfBuckets = scanner.nextInt();
                 if (numOfBuckets <= 0) {
-                    System.out.println("Количество корзин должно быть положительным целым числом. Попробуйте снова."); // Поменял вывод на русский
+                    System.out.println("Количество корзин должно быть положительным целым числом. Попробуйте снова."); // Изменение вывода на русский
                 } else {
                     break;
                 }
             } catch (InputMismatchException e) {
-                System.out.println("Пожалуйста, введите корректное целое число для количества корзин."); // Поменял вывод на русский
+                System.out.println("Пожалуйста, введите корректное целое число для количества корзин."); // Изменение вывода на русский
                 scanner.next();
             }
         }
 
-        System.out.println("\nПеред сортировкой:"); // Поменял вывод на русский
+        System.out.println("\nПеред сортировкой:"); // Изменение вывода на русский
         BucketSort.printArray(array);
 
         BucketSort.bucketSort(array, numOfBuckets);
 
-        System.out.println("\nПосле сортировки корзинами:"); // Поменял вывод на русский
+        System.out.println("\nПосле сортировки корзинами:"); // Изменение вывода на русский
         BucketSort.printArray(array);
     }
 }
